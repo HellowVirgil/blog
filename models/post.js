16793ad0f08fdee3a8f918677355281fd6f8ff04
Virgil <1135895008@qq.com>
2015-05-26 19:28:14 +0800

    add function of paging and comment

--- models/post.js
+++ models/post.js
@@ -26,7 +26,8 @@ Post.prototype.save = function(callback) {
         name: this.name,
         time: time,
         title: this.title,
-        post: this.post
+        post: this.post,
+        comments: []
     };
     //打开数据库
     mongodb.open(function (err, db) {
@@ -53,15 +54,15 @@ Post.prototype.save = function(callback) {
     });
 };
 
-//获取一个人的所有文章（传入参数 name）或获取所有人的文章（不传入参数）
-Post.getAll = function(name, callback) {
+//一次获取十篇文章
+Post.getTen = function(name, page, callback) {
     //打开数据库
     mongodb.open(function (err, db) {
         if (err) {
             return callback(err);
         }
         //读取 posts 集合
-        db.collection('posts', function(err, collection) {
+        db.collection('posts', function (err, collection) {
             if (err) {
                 mongodb.close();
                 return callback(err);
@@ -70,19 +71,25 @@ Post.getAll = function(name, callback) {
             if (name) {
                 query.name = name;
             }
-            //根据 query 对象查询文章
-            collection.find(query).sort({
-                time: -1
-            }).toArray(function (err, docs) {
-                mongodb.close();
-                if (err) {
-                    return callback(err);//失败！返回 err
-                }
-                //解析 markdown 为 html
-                docs.forEach(function (doc) {
-                    doc.post = markdown.toHTML(doc.post);
+            //使用 count 返回特定查询的文档数 total
+            collection.count(query, function (err, total) {
+                //根据 query 对象查询，并跳过前 (page-1)*10 个结果，返回之后的 10 个结果
+                collection.find(query, {
+                    skip: (page - 1)*10,
+                    limit: 10
+                }).sort({
+                    time: -1
+                }).toArray(function (err, docs) {
+                    mongodb.close();
+                    if (err) {
+                        return callback(err);
+                    }
+                    //解析 markdown 为 html
+                    docs.forEach(function (doc) {
+                        doc.post = markdown.toHTML(doc.post);
+                    });
+                    callback(null, docs, total);
                 });
-                callback(null, docs);//成功！以数组形式返回查询的结果
             });
         });
     });
@@ -112,7 +119,12 @@ Post.getOne = function(name, day, title, callback) {
                     return callback(err);
                 }
                 //解析 markdown 为 html
-                doc.post = markdown.toHTML(doc.post);
+                if (doc) {
+                    doc.post = markdown.toHTML(doc.post);
+                    doc.comments.forEach(function (comment) {
+                        comment.content = markdown.toHTML(comment.content);
+                    });
+                }
                 callback(null, doc);//返回查询的一篇文章
             });
         });
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,27 @@
 //crypto 引入模块和 user.js 用户模型文件，crypto 是 Node.js 的一个核心模块，我们用它生成散列值来加密密码
 var crypto = require('crypto'),
     User = require('../models/user.js'),
-    Post = require('../models/post.js');
+    Post = require('../models/post.js'),
+    Comment = require('../models/comment.js');
 
 module.exports = function(app) {
     //主页
     app.get('/', function (req, res) {
-        Post.getAll(null, function (err, posts) {
+        //判断是否是第一页，并把请求的页数转换成 number 类型
+        var page = req.query.p ? parseInt(req.query.p) : 1;
+        //查询并返回第 page 页的 10 篇文章
+        Post.getTen(null, page, function (err, posts, total) {
             if (err) {
                 posts = [];
             }
             res.render('index', {
                 title: '主页',
-                user: req.session.user,
                 posts: posts,
-                //将成功的信息赋值给变量success
+                page: page,
+                isFirstPage: (page - 1) == 0,
+                isLastPage: ((page - 1) * 10 + posts.length) == total,
+                user: req.session.user,
                 success: req.flash('success').toString(),
-                //在渲染 ejs 模版文件时传递这两个变量来进行检测并显示通知
                 error: req.flash('error').toString()
             });
         });
@@ -149,14 +154,15 @@ module.exports = function(app) {
 
     //查询文章
     app.get('/u/:name', function (req, res) {
+        var page = req.query.p ? parseInt(req.query.p) : 1;
         //检查用户是否存在
         User.get(req.params.name, function (err, user) {
             if (!user) {
                 req.flash('error', '用户不存在!');
-                return res.redirect('/');//用户不存在则跳转到主页
+                return res.redirect('/');
             }
-            //查询并返回该用户的所有文章
-            Post.getAll(user.name, function (err, posts) {
+            //查询并返回该用户第 page 页的 10 篇文章
+            Post.getTen(user.name, page, function (err, posts, total) {
                 if (err) {
                     req.flash('error', err);
                     return res.redirect('/');
@@ -164,9 +170,12 @@ module.exports = function(app) {
                 res.render('user', {
                     title: user.name,
                     posts: posts,
-                    user : req.session.user,
-                    success : req.flash('success').toString(),
-                    error : req.flash('error').toString()
+                    page: page,
+                    isFirstPage: (page - 1) == 0,
+                    isLastPage: ((page - 1) * 10 + posts.length) == total,
+                    user: req.session.user,
+                    success: req.flash('success').toString(),
+                    error: req.flash('error').toString()
                 });
             });
         });
@@ -230,6 +239,29 @@ module.exports = function(app) {
             }
             req.flash('success', '删除成功!');
             res.redirect('/');
+        });
+    });
+
+    //评论
+    app.post('/u/:name/:day/:title', function (req, res) {
+        var date = new Date(),
+            time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+                date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+        var comment = {
+            name: req.body.name,
+            email: req.body.email,
+            website: req.body.website,
+            time: time,
+            content: req.body.content
+        };
+        var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+        newComment.save(function (err) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('back');
+            }
+            req.flash('success', '留言成功!');
+            res.redirect('back');
         });
     });
 
