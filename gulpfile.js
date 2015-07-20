2b2830dbee83f0dab140cd500fc5a1b50f1aeb97
yuntan.yg <yuntan.yg@alibaba-inc.com>
2015-07-20 15:45:06 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    less = require('gulp-less');
+
+gulp.task('less',function(){
+    gulp.src('public/stylesheets/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('public/stylesheets'))
+});
+
+gulp.task('watch',function(){
+    gulp.watch('public/stylesheets/*.less',['less']);
+})
